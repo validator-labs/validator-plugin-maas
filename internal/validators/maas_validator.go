9d0b6ca7e0cd49b861a8989dbe4da75725a7239d
Oz Tiram <oz@spectrocloud.com>
2024-02-01 11:30:00 +0100

    First attempt at actually validating an image rule
    Signed-off-by: Oz Tiram <oz@spectrocloud.com>

--- internal/validators/maas_validator.go
+++ internal/validators/maas_validator.go
@@ -1,11 +1,13 @@
 package validators
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/go-logr/logr"
 
 	maasclient "github.com/maas/gomaasclient/client"
+	"github.com/maas/gomaasclient/entity"
 
 	"github.com/spectrocloud-labs/validator-plugin-maas/api/v1alpha1"
 	"github.com/spectrocloud-labs/validator-plugin-maas/internal/constants"
@@ -29,10 +31,25 @@ func NewMaasRuleService(log logr.Logger) *MaasRuleService {
 func (s *MaasRuleService) ReconcileMaasInstanceRule(imgRule v1alpha1.OSImage, mc *maasclient.Client) (*vapitypes.ValidationResult, error) {
 	vr := buildValidationResult(imgRule)
 
+	errMsg := "failed to validate rule"
 	errs := make([]error, 0)
 	details := make([]string, 0)
 
-	errMsg := "failed to validate rule"
+	brs, err := s.listOSImages(mc)
+	if err != nil {
+		return vr, err
+	}
+	var found bool = false
+	for _, br := range brs {
+		if (imgRule.Name == br.Name) && (imgRule.Architecture == br.Architecture) {
+			found = true
+			break
+		}
+	}
+	if !found {
+		errs = append(errs, errors.New(errMsg))
+		details = append(details, fmt.Sprintf("OS image %s with arch %s was not found", imgRule.Name, imgRule.Architecture))
+	}
 	s.updateResult(vr, errs, errMsg, imgRule.Name, details...)
 
 	if len(errs) > 0 {
@@ -64,4 +81,17 @@ func (s *MaasRuleService) updateResult(vr *types.ValidationResult, errs []error,
 	for _, detail := range details {
 		vr.Condition.Details = append(vr.Condition.Details, detail)
 	}
+}
+
+func (s *MaasRuleService) listOSImages(mc *maasclient.Client) ([]entity.BootResource, error) {
+	bootResoursces := make([]entity.BootResource, 0)
+	readEntity := entity.BootResourcesReadParams{}
+	br, err := mc.BootResources.Get(&readEntity)
+	if err != nil {
+		return bootResoursces, err
+	}
+	return br, nil
+	//for _, b := range br {
+	//	fmt.Println(b.Architecture, b.Name)
+	//}
 }
