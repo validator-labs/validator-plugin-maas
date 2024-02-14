06b19a0fc66e80dd7b06f7aeb1bab91b62c1e5fc
Oz Tiram <oz@spectrocloud.com>
2024-02-14 21:39:52 +0100

    Re-introduce ginkgo skeleton
    Signed-off-by: Oz Tiram <oz@spectrocloud.com>

--- internal/controller/maasvalidator_controller_test.go
+++ internal/controller/maasvalidator_controller_test.go
@@ -1,5 +1,19 @@
 package controller
 
-//+kubebuilder:scaffold:imports
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	//+kubebuilder:scaffold:imports
+)
 
 const MaasValidatorName = "maas-validator"
+
+var _ = Describe("MaaSValidator controller", Ordered, func() {
+
+	BeforeEach(func() {
+		// toggle true/false to enable/disable the OCIValidator controller specs
+		if false {
+			Skip("skipping")
+		}
+	})
+}
