47bd179fd78d581e5e358190ac137a5500588d03
Oz Tiram <oz.tiram@gmail.com>
2024-02-14 13:29:03 +0100

    Add first test case
    Signed-off-by: Oz Tiram <oz.tiram@gmail.com>

--- internal/validators/maas_validator_test.go
+++ internal/validators/maas_validator_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/maas/gomaasclient/entity"
+	"github.com/spectrocloud-labs/validator-plugin-maas/api/v1alpha1"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -16,13 +17,41 @@ func (d *DummyMaaSAPIClient) ListDNSServers() ([]entity.DNSResource, error) {
 }
 
 func (d *DummyMaaSAPIClient) ListOSImages() ([]entity.BootResource, error) {
-	d.images = make([]entity.BootResource, 0)
+
 	return d.images, nil
 }
 
 func TestFindingBootResources(t *testing.T) {
-	maasRuleService := NewMaasRuleService(&DummyMaaSAPIClient{})
 
-	images, _ := maasRuleService.ListOSImages()
-	assert.Equal(t, len(images), 0)
+	type TestCase struct {
+		ruleService *MaasRuleService
+		imageRule   v1alpha1.OSImage
+		errors      []error
+		details     []string
+	}
+
+	tc := TestCase{
+		ruleService: NewMaasRuleService(&DummyMaaSAPIClient{
+			images: []entity.BootResource{
+				{
+					Name:         "Ubuntu",
+					Architecture: "amd64/ga-20.04",
+				},
+			},
+		}),
+		imageRule: v1alpha1.OSImage{
+			Name:         "Ubuntu",
+			Architecture: "amd64/ga-20.04",
+		},
+		errors:  make([]error, 0),
+		details: make([]string, 0),
+	}
+
+	images, _ := tc.ruleService.ListOSImages()
+	assert.Equal(t, len(images), 1)
+
+	errors, details := findBootResources(tc.imageRule, images)
+
+	assert.Equal(t, errors, tc.errors)
+	assert.Equal(t, details, tc.details)
 }
