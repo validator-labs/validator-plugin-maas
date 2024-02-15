ece75f616297f91fd35e09029903ed38bf41fc94
Oz Tiram <oz.tiram@gmail.com>
2024-02-15 11:51:33 +0100

    Add basic integration testing
    This test revealed that we didn't care if the API client is nil, which
    caused the controller to crash when trying to list OS images.
    
    Signed-off-by: Oz Tiram <oz.tiram@gmail.com>

--- internal/controller/maasvalidator_controller_test.go
+++ internal/controller/maasvalidator_controller_test.go
@@ -1,9 +1,18 @@
 package controller
 
 import (
+	"context"
+
 	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+
 	// . "github.com/onsi/gomega"
 	//+kubebuilder:scaffold:imports
+	"github.com/spectrocloud-labs/validator-plugin-maas/api/v1alpha1"
+	vapi "github.com/spectrocloud-labs/validator/api/v1alpha1"
 )
 
 const MaasValidatorName = "maas-validator"
@@ -15,5 +24,48 @@ var _ = Describe("MaaSValidator controller", Ordered, func() {
 		if false {
 			Skip("skipping")
 		}
+	})
+
+	val := &v1alpha1.MaasValidator{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      MaasValidatorName,
+			Namespace: validatorNamespace,
+		},
+		Spec: v1alpha1.MaasValidatorSpec{
+			MaasInstance: v1alpha1.MaasInstance{
+				Host: "maas.sc",
+				Auth: v1alpha1.Auth{
+					SecretName: "maas-api-token",
+				},
+			},
+			MaasInstanceRules: v1alpha1.MaasInstanceRules{
+				Name: "validate ubuntu images",
+				OSImages: []v1alpha1.OSImage{
+					{Name: "Ubuntu", Architecture: "amd64/ga-20.04"},
+				},
+			},
+		},
+	}
+
+	//secret := &corev1.Secret{}
+
+	vr := &vapi.ValidationResult{}
+	vrKey := types.NamespacedName{Name: validationResultName(val), Namespace: validatorNamespace}
+
+	It("Should create a ValidationResult and update its Status with a failed condition", func() {
+		By("By creating a new MaasValidator")
+		ctx := context.Background()
+
+		Expect(k8sClient.Create(ctx, val)).Should(Succeed())
+
+		// Wait for the ValidationResult's Status to be updated
+		Eventually(func() bool {
+			if err := k8sClient.Get(ctx, vrKey, vr); err != nil {
+				return false
+			}
+
+			stateOk := vr.Status.State == vapi.ValidationFailed
+			return stateOk
+		}, timeout, interval).Should(BeTrue(), "failed to create a ValidationResult")
 	})
 })
