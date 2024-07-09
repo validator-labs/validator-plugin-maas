9f6569e1bf9123d614a80b06f0fbe8a7ac6c5eb3
Ahmad Ibrahim <ahm.ibr@hotmail.com>
2024-07-09 14:22:20 -0700

    chore: fix typo

--- cmd/main.go
+++ cmd/main.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Package main initializes an MaasValidator controller.
+// Package main initializes a MaasValidator controller.
 package main
 
 import (
